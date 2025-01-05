5e04e91041fb49f8c392217cb2d6186579a584bf
nicomyerr <mail@nicomeyer.me>
2025-01-05 18:12:14 +0100

    feat: dynamic size of text bubble

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,19 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let text_bubble = format!("________________\n< {} >\n----------------\n", args.text);
+    let text_bubble = text_bubble(args.text);
     let totoro = fs::read_to_string("resources/totoro.txt").expect("Unable to read file");
     println!("{}{}", text_bubble, totoro);
+}
+
+fn text_bubble(text: String) -> String {
+    let mut top = String::from(" __");
+    let mut bot = String::from(" --");
+    let mut i = 0;
+    while i < text.len() {
+        top.push('_');
+        bot.push('-');
+        i += 1;
+    }
+    return format!("{}\n< {} > \n{}\n", top, text, bot);
 }
