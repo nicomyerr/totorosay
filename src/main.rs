21ce6146d61a872d6981c5cee38a14a0b03245a6
nicomyerr <mail@nicomeyer.me>
2025-01-12 20:49:50 +0100

    fix: refactoring

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,19 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+
     let text = args.text.join(" ");
-    let text_bubble = text_bubble(text);
-    // TODO: refactoring
-    // TODO: handle longer input with linebreaks
-    let mut file = String::from("resources/totoro.txt");
-    if args.big {
-        file = file.replace("totoro", "totoro-big");
-    }
-    let totoro = fs::read_to_string(file).expect("Unable to read file");
+    let text_bubble = text_bubble(&text);
+
+    let path = "resources/".to_string();
+    let file = size_to_file(args.big);
+    let totoro = fs::read_to_string(path + &file).expect("Unable to read file");
+
     println!("{}{}", text_bubble, totoro);
 }
 
-fn text_bubble(text: String) -> String {
+fn text_bubble(text: &str) -> String {
+    // TODO: handle longer input with linebreaks
     let mut top = String::from(" __");
     let mut bot = String::from(" --");
     let mut i = 0;
@@ -35,4 +35,11 @@ fn text_bubble(text: String) -> String {
         i += 1;
     }
     return format!("{}\n< {} > \n{}\n", top, text, bot);
+}
+
+fn size_to_file(big: bool) -> String {
+    return match big {
+        true => "totoro-big.txt".to_string(),
+        false => "totoro.txt".to_string(),
+    };
 }
