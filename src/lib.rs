2165f8f72abb209e82577b3574dcff9b61846b27
nicomyerr <mail@nicomeyer.me>
2025-03-13 20:45:36 +0100

    refactor: added lib

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::fs;
+
+pub fn text_bubble(text: &str) -> String {
+    // TODO: handle longer input with linebreaks
+    let mut top = String::from(" __");
+    let mut bot = String::from(" --");
+    let mut i = 0;
+    while i < text.len() {
+        top.push('_');
+        bot.push('-');
+        i += 1;
+    }
+    return format!("{}\n< {} > \n{}\n", top, text, bot);
+}
+
+pub fn get_totoro_ascii(big: bool) -> String {
+    let path = "resources/".to_string();
+    let file = size_to_file(big);
+    return fs::read_to_string(path + &file).expect("Unable to read file");
+}
+
+// TODO: private function
+pub fn size_to_file(big: bool) -> String {
+    return match big {
+        true => "totoro-big.txt".to_string(),
+        false => "totoro.txt".to_string(),
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use std::fs;
 
 #[derive(Parser)]
 #[command(version)]
@@ -15,36 +14,9 @@ fn main() {
     let args = Args::parse();
 
     let text = args.text.join(" ");
-    let text_bubble = text_bubble(&text);
+    let text_bubble = totorosay::text_bubble(&text);
 
-    let totoro = get_totoro_ascii(args.big);
+    let totoro = totorosay::get_totoro_ascii(args.big);
 
     println!("{}{}", text_bubble, totoro);
-}
-
-fn text_bubble(text: &str) -> String {
-    // TODO: handle longer input with linebreaks
-    let mut top = String::from(" __");
-    let mut bot = String::from(" --");
-    let mut i = 0;
-    while i < text.len() {
-        top.push('_');
-        bot.push('-');
-        i += 1;
-    }
-    return format!("{}\n< {} > \n{}\n", top, text, bot);
-}
-
-pub fn get_totoro_ascii(big: bool) -> String {
-    let path = "resources/".to_string();
-    let file = size_to_file(big);
-    return fs::read_to_string(path + &file).expect("Unable to read file");
-}
-
-// TODO: private function
-pub fn size_to_file(big: bool) -> String {
-    return match big {
-        true => "totoro-big.txt".to_string(),
-        false => "totoro.txt".to_string(),
-    };
 }
